a29fc7eb2394fd6039d7d884602ba0669a30db0b
mpwagner1 <94921116+mpwagner1@users.noreply.github.com>
2025-06-16 13:13:32 -0500

    Update test_subject_module.py

--- ingestion/arkhamanalytics/test_subject_module.py
+++ ingestion/arkhamanalytics/test_subject_module.py
@@ -51,3 +51,5 @@ def square(x: float) -> float:
 def is_anagram(word1: str, word2: str) -> bool:
     """Check if two strings are anagrams of each other."""
     return sorted(word1.lower()) == sorted(word2.lower())
+
+#test
